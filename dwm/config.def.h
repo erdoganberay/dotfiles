cfa49d757bd17a2e70e0a067b1bd65357e80cd50
erdoganberay <berayerdogn@gmail.com>
2026-03-18 01:56:07 -0400

    picom

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -6,10 +6,10 @@
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 16;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 16;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 16;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 19;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 19;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 19;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 19;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
