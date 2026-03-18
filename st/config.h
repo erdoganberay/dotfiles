42a2661c861f901238ddac02620a4ac985f1edd8
erdoganberay <berayerdogn@gmail.com>
2026-03-18 01:37:06 -0400

    stable setup before picom

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font:size=13:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font:size=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
